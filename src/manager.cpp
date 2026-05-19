de1e4e73ccbc5c1632050c021b3be1ead0df9295
hexne <printfne@gmail.com>
2026-05-19 19:34:37 +0800

    Manager 重写，过编译

--- src/manager.cpp
+++ src/manager.cpp
@@ -2,81 +2,116 @@ import std;
 import client;
 import net;
 
-std::vector<std::unique_ptr<Client>> clients;
-
-struct Account {
-    std::string name;
-    std::string number;
-    std::string password;
+// std::vector<std::unique_ptr<Client>> clients;
+//
+// struct Account {
+//     std::string name;
+//     std::string number;
+//     std::string password;
+// };
+//
+// Account get_account(int number) {
+//     return Account {
+//         .name = std::format("user{}", number),
+//         .number = std::format("num{}", number),
+//         .password = std::format("pass{}", number),
+//     };
+// }
+//
+//
+// void create_client(std::span<char> arg) {
+//     int number = std::stoi(std::string(arg.data(), arg.size()));
+//     clients.reserve(number);
+//     for (int i = 0; i < number; i++) {
+//         clients.emplace_back(std::make_unique<Client>(Address{"127.0.0.1", 8080}));
+//     }
+//     std::cout << "client : " << clients.size() << std::endl;
+// }
+//
+// void login(std::span<char> arg) {
+//     int number = std::stoi(std::string(arg.data(), arg.size()));
+//     auto count = std::min(static_cast<std::size_t>(number), clients.size());
+//     std::size_t success = 0;
+//
+//     for (std::size_t i = 0; i < count; i++) {
+//         auto account = get_account(static_cast<int>(i + 1));
+//         if (clients[i]->login(account.number, account.password))
+//             success++;
+//     }
+//
+//     std::cout << "login : " << success << "/" << count << std::endl;
+// }
+//
+// void reg(std::span<char> arg) {
+//     int number = std::stoi(std::string(arg.data(), arg.size()));
+//
+//     for (std::size_t i = 0; i < number; i++) {
+//         auto account = get_account(static_cast<int>(i + 1));
+//         clients[i]->register_user(account.name, account.number, account.password);
+//     }
+// }
+//
+struct UserStatus {
+    int id{};
+    std::string name{};
+    std::string status{};
 };
 
-Account get_account(int number) {
-    return Account {
-        .name = std::format("user{}", number),
-        .number = std::format("num{}", number),
-        .password = std::format("pass{}", number),
-    };
-}
+std::vector<UserStatus> users;
 
 
-void create_client(std::span<char> arg) {
-    int number = std::stoi(std::string(arg.data(), arg.size()));
-    clients.reserve(number);
-    for (int i = 0; i < number; i++) {
-        clients.emplace_back(std::make_unique<Client>(Address{"127.0.0.1", 8080}));
-    }
-    std::cout << "client : " << clients.size() << std::endl;
-}
 
-void login(std::span<char> arg) {
-    int number = std::stoi(std::string(arg.data(), arg.size()));
-    auto count = std::min(static_cast<std::size_t>(number), clients.size());
-    std::size_t success = 0;
+void show_user_status() {
+    std::println("{:^20}{:^20}{:^20}", "id", "name", "status");
 
-    for (std::size_t i = 0; i < count; i++) {
-        auto account = get_account(static_cast<int>(i + 1));
-        if (clients[i]->login(account.number, account.password))
-            success++;
-    }
-
-    std::cout << "login : " << success << "/" << count << std::endl;
-}
-
-void reg(std::span<char> arg) {
-    int number = std::stoi(std::string(arg.data(), arg.size()));
-
-    for (std::size_t i = 0; i < number; i++) {
-        auto account = get_account(static_cast<int>(i + 1));
-        clients[i]->register_user(account.name, account.number, account.password);
+    for (auto user : users) {
+        std::println("{:^20}{:^20}{:^20}", user.id, user.name, user.status);
     }
 }
-
 
 int main(int argc, char *argv[]) {
-
-    std::map<std::string, std::function<void(std::span<char>)>> events = {
-        { "create", create_client },
-        { "reg", reg },
-        { "login", login }
-    };
-
+    // std::thread thread(show_user_status);
 
     std::string line;
+
+    std::print(":");
     while (std::getline(std::cin, line)) {
-        if (line.empty())
-            continue;
-
-        auto pos = line.find(' ');
-        auto cmd = line.substr(0, pos);
-
-        if (!events.contains(cmd)) {
-            std::cout << "unknown command: " << cmd << std::endl;
-            continue;
+        if (line == "quit" || line == "q" || line == "exit") {
+            break;
         }
+        if (line == "h" || line == "help") {  }
 
-        auto arg_start = pos == std::string::npos ? line.size() : pos + 1;
-        events[cmd](std::span{line.data() + arg_start, line.size() - arg_start});
+        else if (line == "show") {
+            show_user_status();
+        }
+        std::print(":");
     }
+    // thread.join();
+
+    // std::map<std::string, std::function<void(std::span<char>)>> events = {
+    //     { "create", create_client },
+    //     { "reg", reg },
+    //     { "login", login }
+    // };
+    //
+    //
+    // std::string line;
+    // while (std::getline(std::cin, line)) {
+    //     if (line.empty())
+    //         continue;
+    //
+    //     auto pos = line.find(' ');
+    //     auto cmd = line.substr(0, pos);
+    //
+    //     if (!events.contains(cmd)) {
+    //         std::cout << "unknown command: " << cmd << std::endl;
+    //         continue;
+    //     }
+    //
+    //     auto arg_start = pos == std::string::npos ? line.size() : pos + 1;
+    //     events[cmd](std::span{line.data() + arg_start, line.size() - arg_start});
+    // }
+
 
     return 0;
 }
--- src/room.cppm
+++ src/room.cppm
@@ -7,56 +7,56 @@ module;
 export module room;
 import std;
 
-// 房间
-enum class RoomStatus {
-    free,
-    match,
-    gaming
-};
-export class RoomList;
-export class Room {
-    int room_id{};
-    std::vector<int> users_{};
-    RoomStatus status{};
-    friend class RoomList;
-public:
-    explicit Room(const int id) : room_id(id) {  }
-    void add_user(const int id) {
-        if (std::ranges::find(users_, id) == users_.end())
-            users_.push_back(id);
-    }
-    void remove_user(const int id) {
-        if (const auto it = std::ranges::find(users_, id); it != users_.end()) {
-            std::swap(*it, users_.back());
-            users_.pop_back();
-        }
-    }
-    void start_match() {
-
-    }
-
-    void stop_match() {
-    }
-};
-
-
-class RoomList {
-    // 管理rides
-    std::unordered_map<int, Room> rooms_;
-    static inline int rooms_id = 1;
-
-    Room &create_room() {
-        // @TODO, 生成一个id
-        const int id = rooms_id++;
-        rooms_[id] = Room(id);
-        return rooms_[id];
-    }
-
-    std::vector<Room> match() {
-        // 根据一系列结果返回匹配队列
-
-
-        return {};
-    }
-
-};
+// // 房间
+// enum class RoomStatus {
+//     free,
+//     match,
+//     gaming
+// };
+// export class RoomList;
+// export class Room {
+//     int room_id{};
+//     std::vector<int> users_{};
+//     RoomStatus status{};
+//     friend class RoomList;
+// public:
+//     explicit Room(const int id) : room_id(id) {  }
+//     void add_user(const int id) {
+//         if (std::ranges::find(users_, id) == users_.end())
+//             users_.push_back(id);
+//     }
+//     void remove_user(const int id) {
+//         if (const auto it = std::ranges::find(users_, id); it != users_.end()) {
+//             std::swap(*it, users_.back());
+//             users_.pop_back();
+//         }
+//     }
+//     void start_match() {
+//
+//     }
+//
+//     void stop_match() {
+//     }
+// };
+//
+//
+// class RoomList {
+//     // 管理rides
+//     std::unordered_map<int, Room> rooms_;
+//     static inline int rooms_id = 1;
+//
+//     Room &create_room() {
+//         // @TODO, 生成一个id
+//         const int id = rooms_id++;
+//         rooms_[id] = Room(id);
+//         return rooms_[id];
+//     }
+//
+//     std::vector<Room> match() {
+//         // 根据一系列结果返回匹配队列
+//
+//
+//         return {};
+//     }
+//
+// };
