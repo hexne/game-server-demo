0ce56ab4f11009bbe92ec44d1505570f7ed6693e
hexne <printfne@gmail.com>
2026-05-19 22:24:27 +0800

    扩展manager

--- src/manager.cpp
+++ src/manager.cpp
@@ -1,6 +1,7 @@
 import std;
 import client;
 import net;
+import range;
 
 // std::vector<std::unique_ptr<Client>> clients;
 //
@@ -19,14 +20,14 @@ Account get_account(int number) {
 }
 
 
-void create_client(std::span<char> arg) {
-    int number = std::stoi(std::string(arg.data(), arg.size()));
-    clients.reserve(number);
-    for (int i = 0; i < number; i++) {
-        clients.emplace_back(std::make_unique<Client>(Address{"127.0.0.1", 8080}));
-    }
-    std::cout << "client : " << clients.size() << std::endl;
-}
+// void create_client(std::span<char> arg) {
+//     int number = std::stoi(std::string(arg.data(), arg.size()));
+//     clients.reserve(number);
+//     for (int i = 0; i < number; i++) {
+//         clients.emplace_back(std::make_unique<Client>(Address{"127.0.0.1", 8080}));
+//     }
+//     std::cout << "client : " << clients.size() << std::endl;
+// }
 //
 // void login(std::span<char> arg) {
 //     int number = std::stoi(std::string(arg.data(), arg.size()));
@@ -57,40 +58,77 @@ struct UserStatus {
     std::string status{};
     std::unique_ptr<Client> client;
 };
+class ClientManager {
+    std::unordered_map<int, Account> users_;
+    Epoll epoll_;
+    bool stop_;
+public:
+    void epoll_loop() {
 
-std::vector<UserStatus> users;
+        while (stop_) {
 
+        }
 
-
-void show_user_status() {
-    users[0].client->
-    std::println("{:^20}{:^20}{:^20}", "id", "name", "status");
-
-    for (auto user : users) {
-        std::println("{:^20}{:^20}{:^20}", user.id, user.name, user.status);
     }
+
+    void show() {
+        std::println("{:^10}{:^10}{:^20}{:^20}{:^20}", "index", "fd", "id", "name", "status");
+        for (int i = 1;i <= users_.size(); ++i) {
+            auto &user = users_[i];
+            std::println("{:^10}{:^10}{:^20}{:^20}{:^20}", i, "/", "/", user.name, "/");
+
+        }
+        // for (auto &[fd, user] : users_) {
+            // std::println("{:^10}{:^10}{:^20}{:^20}{:^20}", fd, "/", user.name, "/");
+        // }
+    }
+
+    void add(int number = 1) {
+        static int index = 1;
+        for (auto i : Range(number)) {
+            users_.insert(std::make_pair(index , get_account(index)));
+            index += 1;
+        }
+    }
+
+    void stop() {
+        stop_ = true;
+    }
+
+};
+
+
+
+void epoll_thread() {
+
 }
+
 
 int main(int argc, char *argv[]) {
     // std::thread thread(show_user_status);
+    ClientManager manager;
+    std::jthread thread(&ClientManager::epoll_loop, &manager);
 
     std::string cmd;
 
     std::print(":");
     while (std::cin >> cmd) {
         if (cmd == "quit" || cmd == "q" || cmd == "exit") {
+            manager.stop();
+            thread.request_stop();
             break;
         }
         if (cmd == "h" || cmd == "help") {  }
 
         else if (cmd == "show") {
-            show_user_status();
+            manager.show();
         }
+        // add number, 添加number个客户端
+        // @TODO，add 的时候就应该创建fd,而不是在login的时候
         else if (cmd == "add") {
             int number;
             std::cin >> number;
-
-            // 创建多少个用户
+            manager.add(number);
         }
         else if (cmd == "up") {
             std::vector<int> ids{};
@@ -112,6 +150,7 @@ int main(int argc, char *argv[]) {
 
         std::print(":");
     }
+    thread.join();
 
     return 0;
 }
